2e88bd374eb21f3e144e248cc3f8f13566cce14a
tsrgit <trgit@hoghill.net>
2019-11-23 22:06:06 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# conditions5e
+# Displaying D&D 5e Conditions
+This module alters the icons in the Status Effects panel of the Token Hub to reflect the possible "conditions" in D&D 5e.  It also limits the use of the effects overlay to displaying whether the token is seriously wounded, unconscious, or dead. The module automatically uses a red `wounded` overlay when HP falls below 50%.  When HP falls to zero, the display automatically switches to a yellow `unconscious` overlay.  At zero HP, the overlay can be toggled between `unconscious` and `dead` using either the Token HUD or the `Mark Defeated` icon in the Combat Tracker.
+  
+# Installation
+See https://github.com/foundry-vtt-community/wiki/wiki/Modules#installing-modules. Open the Add-on Modules tab in the Configuration and Setup dialog. Click Install Module, paste `https://raw.githubusercontent.com/trdischat/conditions5e/master/module.json` in as the Manifest URL, then click Install.
+
+As DM go to the `Manage Modules` options menu in the Game Settings for your World, then enable the `D&D 5e Conditions` module.
+
+# License
+This Foundry VTT module, written by trdischat, is licensed under a [Creative Commons Attribution 4.0 International License](http://creativecommons.org/licenses/by/4.0/).  Icons from game-icons.net are licensed under a [Creative Commons Attribution 3.0 International License](http://creativecommons.org/licenses/by/3.0/).  Icons from svgrepo.com are licensed under a [Creative Commons Attribution 4.0 International License](http://creativecommons.org/licenses/by/4.0/).  Icons from flaticon.com are licensed under the [Flaticon Basic License](https://file000.flaticon.com/downloads/license/license.pdf).
+
+This work is licensed under Foundry Virtual Tabletop [EULA - Limited License Agreement for module development v 0.1.6](http://foundryvtt.com/pages/license.html).
---
+++ main.js
@@ -0,0 +1,131 @@
+/**
+ * An array of status effect icons which can be applied to Tokens
+ * @type {Array}
+ */
+CONFIG.statusEffects = [
+  "modules/conditions5e/icons/dead.svg",
+  "modules/conditions5e/icons/unconscious.svg",
+  "modules/conditions5e/icons/stunned.svg",
+  "modules/conditions5e/icons/exhaustion1.svg",
+
+  "modules/conditions5e/icons/incapacitated.svg",
+  "modules/conditions5e/icons/paralyzed.svg",
+  "modules/conditions5e/icons/petrified.svg",
+  "modules/conditions5e/icons/exhaustion2.svg",
+
+  "modules/conditions5e/icons/grappled.svg",
+  "modules/conditions5e/icons/restrained.svg",
+  "modules/conditions5e/icons/prone.svg",
+  "modules/conditions5e/icons/exhaustion3.svg",
+
+  "modules/conditions5e/icons/charmed.svg",
+  "modules/conditions5e/icons/frightened.svg",
+  "modules/conditions5e/icons/poisoned.svg",
+  "modules/conditions5e/icons/exhaustion4.svg",
+
+  "modules/conditions5e/icons/blinded.svg",
+  "modules/conditions5e/icons/deafened.svg",
+  "modules/conditions5e/icons/diseased.svg",
+  "modules/conditions5e/icons/exhaustion5.svg"
+];
+  
+// Condition Types
+CONFIG.conditionTypes = {
+  "blinded": "Blinded",
+  "charmed": "Charmed",
+  "dead": "Dead",
+  "deafened": "Deafened",
+  "diseased": "Diseased",
+  "exhaustion": "Exhaustion",
+  "exhaustion1": "Exhaustion Level 1",
+  "exhaustion2": "Exhaustion Level 2",
+  "exhaustion3": "Exhaustion Level 3",
+  "exhaustion4": "Exhaustion Level 4",
+  "exhaustion5": "Exhaustion Level 5",
+  "frightened": "Frightened",
+  "grappled": "Grappled",
+  "incapacitated": "Inacapacitated",
+  "invisible": "Invisible",
+  "paralyzed": "Paralyzed",
+  "petrified": "Petrified",
+  "poisoned": "Poisoned",
+  "prone": "Prone",
+  "restrained": "Restrained",
+  "stunned": "Stunned",
+  "unconscious": "Unconscious",
+  "wounded": "Wounded"
+};
+
+  /**
+   * The control icons used for rendering common HUD operations
+   * @type {Object}
+   */
+CONFIG.controlIcons = {
+    combat: "icons/svg/combat.svg",
+    visibility: "modules/conditions5e/icons/invisible.svg",
+    effects: "icons/svg/aura.svg",
+    lock: "icons/svg/padlock.svg",
+    up: "icons/svg/up.svg",
+    down: "icons/svg/down.svg",
+    defeated: "modules/conditions5e/icons/dead.svg"
+  };
+  
+/**
+ * Utility function used by patch functions to alter specific lines in a class
+ * @param {Class} klass           Class to be patched
+ * @param {Function} func         Function in the class to be patched
+ * @param {Number} line_number    Line within the function to be patched
+ * @param {String} line           Existing text of line to be patched
+ * @param {String} new_line       Replacement text for line to be patched
+ * @returns {Class}              Revised class
+ */
+function patchClass(klass, func, line_number, line, new_line) {
+  let funcStr = func.toString()
+  let lines = funcStr.split("\n")
+  if (lines[line_number].trim() == line.trim()) {
+    lines[line_number] = lines[line_number].replace(line, new_line);
+    classStr = klass.toString()
+    fixedClass = classStr.replace(funcStr, lines.join("\n"))
+    return Function('"use strict";return (' + fixedClass + ')')();
+  }
+  else {
+    console.log("Function has wrong content at line ", line_number, " : ", lines[line_number].trim(), " != ", line.trim(), "\n", funcStr)
+  }
+}
+
+function patchCombatTrackerClass() {
+  newClass = patchClass(CombatTracker, CombatTracker.prototype._onCombatantControl, 18,
+    `if ( isDefeated && !token.data.overlayEffect ) token.toggleOverlay(CONFIG.controlIcons.defeated);`,
+    `if ( isDefeated && token.data.overlayEffect !== CONFIG.controlIcons.defeated ) token.toggleOverlay(CONFIG.controlIcons.defeated);`);
+  if (!newClass) return;
+  CombatTracker = newClass
+}
+
+if (patchedCombatTrackerClass == undefined) {
+  patchCombatTrackerClass();
+  var patchedCombatTrackerClass = true;
+}
+
+Token.prototype._updateHealthOverlay = function(tok) {
+  let maxHP = tok.actor.data.data.attributes.hp.max;
+  let curHP = tok.actor.data.data.attributes.hp.value;
+  let priorHealth = tok.data.overlayEffect;
+  let newHealth = null;
+  if ( curHP <= 0 ) {
+    if ( priorHealth === "modules/conditions5e/icons/dead.svg" ) newHealth = priorHealth;
+    else newHealth = "modules/conditions5e/icons/almostdead.svg";
+  }
+  else if ( curHP / maxHP < 0.5 ) newHealth = "modules/conditions5e/icons/wounded.svg";
+  if ( newHealth !== priorHealth ) {
+    if ( newHealth === null ) tok.toggleOverlay(priorHealth);
+    else tok.toggleOverlay(newHealth);
+  }
+};
+
+Hooks.on("updateToken", (object, parentId, updated) => {
+  object._updateHealthOverlay(object);
+});
+
+Hooks.on("updateActor", (entity, updated) => {
+  entity.getActiveTokens(true).map(x => x._updateHealthOverlay(x));
+});
